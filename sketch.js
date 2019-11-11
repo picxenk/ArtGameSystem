29dda7630b9c1da4acc64c4796a4e47192b42fc5
Seungbum Kim <picxenk@gmail.com>
2019-11-11 22:12:13 +0900

    test sprite custom collide, draw

--- sketch.js
+++ sketch.js
@@ -2,13 +2,50 @@ let canvas;
 let canvasWidth = 600;
 let canvasHeight = 400;
 
+let walls;
+let balls;
+let left, bottom;
+
 function setup() {
   canvas = createCanvas(canvasWidth, canvasHeight);
   canvas.position(windowWidth/2 - canvasWidth/2, 20);
-  noCursor();
+  // noCursor();
+
+  balls = new Group();
+  walls = new Group();
+  left = createSprite(0, 200, 10, 400);
+  left.immovable = true;
+  bottom = createSprite(300, 400);
+  bottom.immovable = true;
+  walls.add(left);
+  walls.add(bottom);
+
+  bottom.draw = function() {
+    fill(255, 0, 0);
+    rect(0, 0, 600, 10);
+  }
+  bottom.setCollider('rectangle', 0, 0, 600, 10);
+
 }
 
 function draw() {
   background(30);
-  ellipse(mouseX, mouseY, 100, 100);
+
+  for (let i=0; i<balls.length; i++) {
+    balls[i].bounce(bottom);
+    balls[i].bounce(left, killed);
+
+  }
+  drawSprites();
+}
+
+function mousePressed() {
+  var s = createSprite(mouseX, mouseY, 30, 30);
+  s.velocity.x = random(-5, 5);
+  s.velocity.y = random(-5, 5);
+  balls.add(s);
+}
+
+function killed(b, o) {
+  b.remove();
 }
